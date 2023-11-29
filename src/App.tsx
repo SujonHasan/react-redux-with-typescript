c3162cd1f58df1070e6ba87533eb10eddaf1d3f6
SujonHasan <sujonhasan171@gmail.com>
2023-11-29 17:37:21 +0600

    style code added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,16 +8,20 @@ import { actionCreators, State } from './state';
 function App() {
 
   const dispatch = useDispatch();
+  const [open, setOpen] = useState<boolean>(false)
 
   const {depositMoney, withdrawMoney, bankrupt} = bindActionCreators(actionCreators, dispatch);
   const amount = useSelector((state: State) => state.bank)
   
   return (
     <div className="App">
-      <h1>{amount}</h1>
-      <button onClick={() => depositMoney(1000)}>Deposit</button>
-      <button onClick={() => withdrawMoney(500) }>Widthdraw</button>
-      <button onClick={() => bankrupt() }>Bankrupt</button>
+      <h1 className='bg-gray-400 p-10 md:w-1/2 rounded text-5xl my-5 font-bold mx-auto' >{amount}</h1>
+      <div className='flex flex-wrap justify-center gap-5'>
+      <button className='btn  bg-green-700 p-2 rounded-full text-2xl' onClick={() => depositMoney(1000)}>Deposit</button>
+      <button className='btn bg-blue-400 p-2 rounded-full text-2xl' onClick={() => withdrawMoney(500) }>Widthdraw</button>
+      <button className='btn btn bg-red-400 p-2 rounded-full text-2xl' onClick={() => bankrupt() }>Bankrupt</button>
+      </div>
+
     </div>
   );
 }
